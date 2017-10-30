973d60746e5d81bfddb4344911c1fc1f201d371c
Michael Schmid <mschmid@gmx.ch>
2017-10-30 17:08:03 +0100

    Devel. commit

--- download.py
+++ download.py
@@ -1,16 +1,23 @@
 import urllib3
 
-INDEX_URL = "https://ftp.ncbi.nlm.nih.gov/genomes/refseq/archaea/assembly_summary.txt"
-INDEX_PATH = "download/assembly_summary.txt"
+INDEX_URL_BACT = "https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt"
+INDEX_PATH_BACT = "download/bacteria_assembly_summary.txt"
+INDEX_URL_ARCH = "https://ftp.ncbi.nlm.nih.gov/genomes/refseq/archaea/assembly_summary.txt"
+INDEX_PATH_ARCH = "download/archarea_assembly_summary.txt"
+INDEX_URL_VIRAL = "https://ftp.ncbi.nlm.nih.gov/genomes/refseq/viral/assembly_summary.txt"
+INDEX_PATH_VIRAL = "download/viral_assembly_summary.txt"
+
+INDEX = [(INDEX_URL_BACT,INDEX_PATH_BACT),(INDEX_URL_ARCH,INDEX_PATH_ARCH),(INDEX_URL_VIRAL,INDEX_PATH_VIRAL)]
 
 http = urllib3.PoolManager()
-r = http.request('GET', INDEX_URL, preload_content=False)
 
-with open(INDEX_PATH, 'wb') as out:
-    while True:
-        data = r.read()
-        if not data:
-            break
-        out.write(data)
+for (index_url,index_path) in INDEX:
+    r = http.request('GET', index_url, preload_content=False)
+    with open(index_path, 'wb') as out:
+        while True:
+            data = r.read()
+            if not data:
+                break
+            out.write(data)
 
 r.release_conn()
