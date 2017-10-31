7392169509aa2149cbf1d3040775ddd0bdc69ab4
Michael Schmid <mschmid@gmx.ch>
2017-10-31 09:07:31 +0100

    Devel. commit

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import urllib3
+import certifi
 
 INDEX_URL_BACT = "https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt"
 INDEX_PATH_BACT = "download/bacteria_assembly_summary.txt"
@@ -9,7 +10,7 @@ INDEX_PATH_VIRAL = "download/viral_assembly_summary.txt"
 
 INDEX = [(INDEX_URL_BACT,INDEX_PATH_BACT),(INDEX_URL_ARCH,INDEX_PATH_ARCH),(INDEX_URL_VIRAL,INDEX_PATH_VIRAL)]
 
-http = urllib3.PoolManager()
+http = urllib3.PoolManager(cert_reqs="CERT_REQUIRED", ca_certs=certifi.where())
 
 for (index_url,index_path) in INDEX:
     r = http.request('GET', index_url, preload_content=False)
