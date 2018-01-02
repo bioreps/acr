5823aaf6d32cb9ef74d73f262a997117a8b901f9
Michael Schmid <mschmid@gmx.ch>
2018-01-02 15:35:37 +0100

    Devel. commit

--- hmm.py
+++ hmm.py
@@ -5,7 +5,7 @@ import global_param
 HMMSEARCH_PATH = "/usr/bin/hmmscan"
 HMM_PATH = "CAS_hmm/"
 HMM_DBS = "CAS_hmm/extracted_dbs/"
-HMM_OUT = "CAS_hmm/hmmsearch_out/"
+HMM_OUT = "CAS_hmm/hmmscan_out/"
 GBK_PATH_BACT = "download_ncbi/bacteria_assembly_summary/"
 GBK_PATH_ARCH = "download_ncbi/archaea_assembly_summary/"
 GBK_PATH_VIRAL = "download_ncbi/viral_assembly_summary/"
@@ -16,29 +16,35 @@ GBK_PATHS = [GBK_PATH_BACT]
 HMM_DB = [("tigr","TIGRFAMs_modelsCRISPR.hmm"),("pfam","Pfam-A_modelsCRISPR.hmm"),("novel","novel_modelsCRISPR.hmm")]
 
 def hmm_clean_folders():
-    for to_remove in glob.glob("{}{}".format(HMM_OUT, "*.hmmsearchFiltered")):
+    # Clean folder from Cas hits
+    for to_remove in glob.glob("{}{}".format(HMM_OUT, "*.hmmscan.cas.filtered")):
         os.remove(to_remove)
-    for to_remove in glob.glob("{}{}".format(HMM_OUT, "*.hmmsearchOut")):
+    for to_remove in glob.glob("{}{}".format(HMM_OUT, "*.hmmscan.cas.out")):
         os.remove(to_remove)
 
+# IDEA: Could we use jackhmmer here?
+
+# WHAT: Runs hmmscan. Protein sequences of all genomes (individually) against all available hmm models of Cas proteins
 def hmm_run(subset=999999):
     for domain in GBK_PATHS:
         for (hmm_name, hmm_path) in HMM_DB:
             subset_count = 0
             for filename in os.listdir(domain):
                 if filename.endswith(".faa.gz"):
-                    filename_out = filename.replace('.faa.gz', '.'+hmm_name+'.hmmsearchOut')
-                    filename_filtered = filename.replace('.faa.gz', '.'+hmm_name+'.hmmsearchFiltered')
+                    filename_out = filename.replace('.faa.gz', '.'+hmm_name+'.hmmscan.cas.out')
+                    filename_filtered = filename.replace('.faa.gz', '.'+hmm_name+'.hmmscan.cas.filtered')
                     if os.path.exists(filename_filtered):
                         continue
+                    # Do a hmmscan run
                     cmd = "zcat {}{} " \
-                          "| {} -E 1e-12 --domE 1e-12 --cpu {} --tblout {}{} {}{} - >> CAS_hmm/hmmsearch_out/text.txt".format(
+                          "| {} -E 1e-15 --domE 1e-15 --cpu {} --tblout {}{} {}{} - >> CAS_hmm/hmmscan_out/text.txt".format(
                         domain, filename,
                         HMMSEARCH_PATH, global_param.NUMBER_OF_THREADS,
                         HMM_OUT, filename_out,
                         HMM_DBS, hmm_path
                     )
                     os.system(cmd)
+                    # kick out the comment lines and just keep the best hit
                     cmd = "sed 's/  */ /g' {}{} | grep -v ^# | awk '!seen[$3]++' >> {}{}".format(
                         HMM_OUT, filename_out,
                         HMM_OUT, filename_filtered)
